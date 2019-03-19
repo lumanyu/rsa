0336361a180c0120d8756945b840d52d90866bbc
mariolu <mariolu@tencent.com>
2019-03-19 18:12:51 +0800

    add substract and multiple operation

--- huge.c
+++ huge.c
@@ -1,5 +1,9 @@
 #include "huge.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <jemalloc/jemalloc.h>
+//#include <bits/string.h>
+#include <string.h>
 
 /**
  * Extend the space for h by 1 and set the LSB of that int
@@ -54,6 +58,153 @@ add (huge *h1, huge *h2)
     }
 }
 
+/**
+ * Go through h and see how many of the left-most bytes anr unused.
+ * Remove them and resize h appropriately
+ */
+
+void contract (huge *h)
+{
+    int i = 0;
+    while (!(h->rep[i]) && (i < h->size)) {
+        i++;
+    }
+    if (i && i < h->size) {
+        unsigned char *tmp = &h->rep[i];
+        h->rep = (unsigned char*) calloc(h->size -i, sizeof(unsigned char));
+        memcpy(h->rep, tmp, h->size - i);
+        h->size -= i;
+    }
+}
+
+static void subtract(huge *h1, huge *h2)
+{
+    int i = h1->size;
+    int j = h2->size;
+    int difference;//signed int - important!
+    unsigned int borrow = 0;
+
+    do {
+        i--;
+        if(j) {
+            j--;
+            difference = h1->rep[i] - h2->rep[j] - borrow;
+        } else {
+            difference = h1->rep[i] - borrow;
+        }
+
+        borrow = (difference < 0);
+        h1->rep[i] = difference;
+    } while (i);
+
+    if (borrow && i) {
+        if (!(h1->rep[i-1])) {// don't borrow i
+            printf("Error, subtraction result is negative\n");
+            exit(0);
+        }
+        h1->rep[i-1] --;
+    }
+    contract(h1);
+}
+
+void copy_huge(huge *tgt, huge *src)
+{
+    if (tgt->rep) {
+        free(tgt->rep);
+    }
+
+    tgt->size = src->size;
+    tgt->rep = (unsigned char*)calloc(src->size, sizeof(unsigned char));
+    memcpy(tgt->rep, src->rep, (src->size * sizeof(unsigned char)));
+}
+
+void free_huge(huge *h)
+{
+    if (h->rep) {
+        free(h->rep);
+    }
+}
+
+void left_shift (huge *h1)
+{
+    int i;
+    int old_carry, carry = 0;
+
+    i = h1->size;
+    do {
+        i--;
+        old_carry = carry;
+        carry = (h1->rep[i] & 0x80) == 0x80;
+        h1->rep[i] = (h1->rep[i] << 1) | old_carry;
+    } while(i);
+
+    if (carry) {
+        expand(h1);
+    }
+}
+
+void set_huge(huge *h, unsigned int val)
+{
+    unsigned int mask, i, shift;
+    h->size = 4;
+
+    //Figure out the minimum amount of space this "val" will take
+    //up in char* (leave at least one byte, though, if "val" is 0)
+    for (mask = 0xFF000000; mask > 0x000000FF; mask >>8) {
+        if (val & mask) {
+            break;
+        }
+        h->size--;
+    }
+
+    h->rep = (unsigned char*) malloc (h->size);
+    //Now work backward through the int, masking off each 8-bit
+    //byte (up to first 0 byte) and copy it into the "huge"
+    //array in big-endian format
+    mask = 0X000000FF;
+    shift = 0;
+    for (i = h->size; i; i--) {
+        h->rep[i-1] = (val &mask) >> shift;
+        mask <<= 8;
+        shift += 8;
+    }
+}
+
+/**
+ * Multiply h1 by h2, overwriting the value of h1
+ */
+
+void multiply (huge *h1, huge *h2)
+{
+    unsigned char mask;
+    unsigned int i;
+    huge temp;
+    set_huge(&temp, 0);
+    copy_huge(&temp, h1);
+
+    set_huge(h1, 0);
+
+    i = h1->size;
+    do {
+        i--;
+        for (mask = 0x01; mask; mask <<=1) {
+            if (mask && h2->rep[i]) {
+                add(h1, &temp);
+            }
+            left_shift(&temp);
+        }
+    } while (i);
+}
+
 int main(void) {
+    char h1[100] = {0x01, 0xFF, 0xFE};
+    char h2[100] = {0x01, 0xFF, 0xFE};
+    huge hh1 = {h1, 100};
+    huge hh2 = {h2, 100};
+    //big endian: x h1->rep, p h1->size
+    add(&hh1, &hh2);
+    subtract(&hh1, &hh2);
+    //printf();
+
     return 0;
 }
